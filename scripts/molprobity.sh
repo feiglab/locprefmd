f98dc106a42c4f0d3b8788f2fe70eda909665005
huhlim <huhlim@gmail.com>
2017-07-10 10:23:07 -0400

    updated molprobity script

--- scripts/molprobity.sh
+++ scripts/molprobity.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
 source $MOLPROBITY/build/setpaths.sh
-convpdb.pl -out generic $1 | pdbelement.pl > /tmp/$$.pdb
-$MOLPROBITY/cmdline/molprobity /tmp/$$.pdb | tail -1 | awk -F: '{print $32}'
-/bin/rm -f /tmp/$$.pdb
+
+TMPDIR=/tmp/molp.$$
+cwd=$(pwd)
+mkdir -p $TMPDIR 
+
+convpdb.pl -out generic $1 | pdbelement.pl > $TMPDIR/$$.pdb
+cd $TMPDIR
+
+$MOLP/cmdline/oneline-analysis . | tail -1 | awk -F: '{print $32}'
+
+cd $cwd
+/bin/rm -rf $TMPDIR
